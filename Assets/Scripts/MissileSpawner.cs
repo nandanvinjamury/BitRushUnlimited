22f1822740391b8842176d2d24d9b68feb9b68df
nandanvinjamury <nvinjamury@gmail.com>
2024-05-05 12:47:33 -0700

    Added missile spawner and adjusted missile speed

--- Assets/Scripts/MissileSpawner.cs
+++ Assets/Scripts/MissileSpawner.cs
@@ -1,13 +1,21 @@
-﻿using UnityEngine;
+﻿using Cysharp.Threading.Tasks;
+using UnityEngine;
 
 namespace bitrush {
     public class MissileSpawner : MonoBehaviour {
+        [SerializeField] private ParticleSystem _spawnParticles;
         [SerializeField] private GameObject _missilePrefab;
 
-        private void Update() {
-            if (Input.GetButtonDown("Fire1")) {
-                Instantiate(_missilePrefab, transform.position, Quaternion.identity);
-            }
+        private void Awake() {
+            InvokeRepeating("ShootMissile", 3f, 5f);
+        }
+
+        private async UniTaskVoid ShootMissile() {
+            _spawnParticles.Play();
+            await UniTask.Delay(500, cancellationToken: this.GetCancellationTokenOnDestroy());
+            _spawnParticles.Stop();
+            Instantiate(_missilePrefab, transform.position, Quaternion.identity);
+            await UniTask.Yield(cancellationToken: this.GetCancellationTokenOnDestroy());
         }
     }
 }
